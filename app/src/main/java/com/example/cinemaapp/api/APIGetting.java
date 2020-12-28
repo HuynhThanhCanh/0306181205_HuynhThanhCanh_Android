4f69225e7de5511a825a9f47a3885f706f4f097d
Nhut <nhatminh785@gmail.com>
2020-12-28 11:11:54 +0700

    - Tạo aysn task

--- app/src/main/java/com/example/cinemaapp/api/APIGetting.java
+++ app/src/main/java/com/example/cinemaapp/api/APIGetting.java
@@ -1,4 +1,27 @@
 package com.example.cinemaapp.api;
 
-public class APIGetting {
+import android.content.Context;
+import android.os.AsyncTask;
+
+public class APIGetting extends AsyncTask<String , String ,  String> {
+    Context context;
+
+    public APIGetting(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
 }
